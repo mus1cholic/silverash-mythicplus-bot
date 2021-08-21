133a48b3d6f5f47cc2e24dda11bb7b12fe28a243
Orion Hsu <oh25@duke.edu>
2021-08-21 16:10:12 -0700

    added basic help command

--- cogs/misc.py
+++ cogs/misc.py
@@ -7,6 +7,16 @@ class Misc(commands.Cog):
     def __init__(self, bot_client):
         self.bot_client = bot_client
 
+    @commands.command()
+    async def help(self, ctx):
+        """
+        Provides a description of all the commands for this bot
+        """
+
+        await ctx.send("***Commands for individuals:***\n" +
+                       "`*score <character>`: Displays the current Raider.IO Mythic+ score of the provided character.\n" +
+                       "")
+
 def setup(bot_client):
     bot_client.add_cog(Misc(bot_client))
 
