f317f7dc9dd2b8f5356a98efb75b88b67cf001cb
Nigel gansta YANG <nigelyang611@hotmail.com>
2021-08-22 16:11:02 -0400

    changed prefix as extension

--- cogs/change_prefix.py
+++ cogs/change_prefix.py
@@ -1,20 +1,25 @@
 import discord
 from discord.ext import commands
 
-bot_client = commands.Bot(command_prefix='.')
+
+class Prefix(commands.Cog):
+    def __init__(self, bot_client):
+        self.bot_client = bot_client
+
+    @commands.command()
+    async def change_prefix(self, ctx, new_prefix):
+        """
+        Changes the command prefix of this bot
+        """
+        if len(new_prefix) == 1:
+            self.bot_client.command_prefix = new_prefix
+            await ctx.send(f"The prefix of this bot has now been changed to {new_prefix}")
+        else:
+            await ctx.send("The entered prefix has a length greater than 1. "
+                           "Please enter a valid prefix")
 
 
-@bot_client.command()
-async def change_prefix(ctx, new_prefix):
-    """
-    Changes the command prefix of this bot
-    """
-    if len(new_prefix) == 1:
-        bot_client.command_prefix = new_prefix
-        await ctx.send(f"The prefix of this bot has now been changed to {new_prefix}.")
-    else:
-        await ctx.send("The entered prefix has a length greater than 1. "
-                       "Please enter a valid prefix")
-    return new_prefix
+def setup(bot_client):
+    bot_client.add_cog(Prefix(bot_client))
 
-bot_client.run('')
+
