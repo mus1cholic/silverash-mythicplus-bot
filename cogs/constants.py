21d6ad6c91f633c577651235879a61bbed5325db
mus1cholic <orionhsu5@hotmail.com>
2021-08-26 17:10:03 -0400

    added best dungeon stat

--- cogs/constants.py
+++ cogs/constants.py
@@ -2,63 +2,171 @@ API_URL = "https://raider.io/api/v1/characters/profile?region={}&realm={}&name={
 
 spec_ids = {
 	'Death Knight': {
-		'Blood': 250,
-		'Frost': 251,
-		'Unholy': 252
+		'Blood': {
+			'id': 0,
+			'uid': 250
+		},
+		'Frost': {
+			'id': 1,
+			'uid': 251
+		},
+		'Unholy': {
+			'id': 2,
+			'uid': 252
+		}
 	},
 	'Demon Hunter': {
-		'Havoc': 577,
-		'Vengeance': 581
+		'Havoc': {
+			'id': 0,
+			'uid': 577
+		},
+		'Vengeance': {
+			'id': 1,
+			'uid': 581
+		}
 	},
 	'Druid': {
-		'Balance': 102,
-		'Feral': 103,
-		'Guardian': 104,
-		'Restoration': 105
+		'Balance': {
+			'id': 0,
+			'uid': 102
+		},
+		'Feral': {
+			'id': 1,
+			'uid': 103
+		},
+		'Guardian': {
+			'id': 2,
+			'uid': 104
+		},
+		'Restoration': {
+			'id': 3,
+			'uid': 105
+		}
 	},
 	'Hunter': {
-		'Beast Mastery': 253,
-		'Marksmanship': 254,
-		'Survival': 255
+		'Beast Mastery': {
+			'id': 0,
+			'uid': 253
+		},
+		'Marksmanship': {
+			'id': 1,
+			'uid': 254
+		},
+		'Survival': {
+			'id': 2,
+			'uid': 255
+		}
 	},
 	'Mage': {
-		'Arcane': 62,
-		'Fire': 63,
-		'Frost': 64
+		'Arcane': {
+			'id': 0,
+			'uid': 62
+		},
+		'Fire': {
+			'id': 1,
+			'uid': 63
+		},
+		'Frost': {
+			'id': 2,
+			'uid': 64
+		}
 	},
 	'Monk': {
-		'Brewmaster': 268,
-		'Windwalker': 269,
-		'Mistweaver': 270
+		'Brewmaster': {
+			'id': 0,
+			'uid': 268
+		},
+		'Windwalker': {
+			'id': 1,
+			'uid': 269
+		},
+		'Mistweaver': {
+			'id': 2,
+			'uid': 270
+		}
 	},
 	'Paladin': {
-		'Holy': 65,
-		'Protection': 66,
-		'Retribution': 70
+		'Holy': {
+			'id': 0,
+			'uid': 65
+		},
+		'Protection': {
+			'id': 1,
+			'uid': 66
+		},
+		'Retribution': {
+			'id': 2,
+			'uid': 70
+		}
 	},
 	'Priest': {
-		'Discipline': 256,
-		'Holy': 257,
-		'Shadow': 258
+		'Discipline': {
+			'id': 0,
+			'uid': 256
+		},
+		'Holy': {
+			'id': 1,
+			'uid': 257
+		},
+		'Shadow': {
+			'id': 2,
+			'uid': 258
+		}
 	},
 	'Rogue': {
-		'Assassination': 259,
-		'Outlaw': 260,
-		'Subtlety': 261
+		'Assassination': {
+			'id': 0,
+			'uid': 259
+		},
+		'Outlaw': {
+			'id': 1,
+			'uid': 260
+		},
+		'Subtlety': {
+			'id': 2,
+			'uid': 261
+		}
 	},
 	'Shaman': {
-		'Elemental': 262,
-		'Enhancement': 263,
-		'Restoration': 264
+		'Elemental': {
+			'id': 0,
+			'uid': 262
+		},
+		'Enhancement': {
+			'id': 1,
+			'uid': 263
+		},
+		'Restoration': {
+			'id': 2,
+			'uid': 264
+		}
 	},
 	'Warlock': {
-		'Affliction': 265,
-		'Demonology': 266,
-		'Destruction': 267
+		'Affliction': {
+			'id': 0,
+			'uid': 265
+		},
+		'Demonology': {
+			'id': 1,
+			'uid': 266
+		},
+		'Destruction': {
+			'id': 2,
+			'uid': 267
+		}
 	},
 	'Warrior': {
-		'Arms': 71,
-		'Fury': 72,
-		'Protection': 73
+		'Arms': {
+			'id': 0,
+			'uid': 71
+		},
+		'Fury': {
+			'id': 1,
+			'uid': 72
+		},
+		'Protection': {
+			'id': 2,
+			'uid': 73
+		}
 	}
 }
--- cogs/individuals.py
+++ cogs/individuals.py
@@ -24,7 +24,7 @@ class Individuals(commands.Cog):
         Sends out the score of the player
         """
 
-        realm, server, character_name = "", "", ""
+        region, realm, character_name = "", "", ""
         api_fields = ['mythic_plus_scores', 'mythic_plus_ranks', 'mythic_plus_best_runs', 'mythic_plus_alternate_runs']
 
         if args == "":
@@ -56,9 +56,9 @@ class Individuals(commands.Cog):
 
         # TODO: use message directly from API response instead of being hardcoded
         try:
-            assert r.status_code != 400, f"character `{character_name}` from `{server}` does not exist, perhaps you misspelled?"
+            assert r.status_code != 400, f"character `{character_name}` from `{realm}` does not exist, perhaps you misspelled?"
         except AssertionError:
-            await ctx.send(f"character `{character_name}` from `{server}` does not exist, perhaps you misspelled?")
+            await ctx.send(f"character `{character_name}` from `{realm}` does not exist, perhaps you misspelled?")
             raise
 
         request_json = r.json()
@@ -69,62 +69,66 @@ class Individuals(commands.Cog):
         char_role = self._convert_role(request_json['active_spec_role'])
         char_class = request_json['class']
         char_spec = request_json['active_spec_name']
+        char_thumbnail = request_json['thumbnail_url']
 
         # scores data
         char_total_score = request_json['mythic_plus_scores']['all']
         char_role_score = request_json['mythic_plus_scores'][char_role]
-        char_spec_score = request_json['mythic_plus_scores']['spec_0']
+        char_spec_score = request_json['mythic_plus_scores'][f'spec_{constants.spec_ids[char_class][char_spec]["id"]}']
 
         # rank data
-        char_role_rank = request_json['mythic_plus_ranks']['dps']
+        char_role_rank = request_json['mythic_plus_ranks'][char_role]
         char_class_rank = request_json['mythic_plus_ranks']['class']
-        char_spec_rank = request_json['mythic_plus_ranks'][f'spec_{constants.spec_ids[char_class][char_spec]}']
+        char_spec_rank = request_json['mythic_plus_ranks'][f'spec_{constants.spec_ids[char_class][char_spec]["uid"]}']
+
+        # TODO: de-duplicate dungeon data code
 
         # individual dungeon data
         best, nw_best, sd_best, halls_best, plaguefall_best, mists_best, spires_best, top_best, dos_best = request_json['mythic_plus_best_runs'], None, None, None, None, None, None, None, None
         for dungeon in best:
-            # breakpoint()
             if dungeon["dungeon"] == "The Necrotic Wake":
-                nw_best = dungeon["dungeon"]
+                nw_best = dungeon
             elif dungeon["dungeon"] == "Sanguine Depths":
-                sd_best = dungeon["dungeon"]
+                sd_best = dungeon
             elif dungeon["dungeon"] == "Spires of Ascension":
-                spires_best = dungeon["dungeon"]
+                spires_best = dungeon
             elif dungeon["dungeon"] == "Mists of Tirna Scithe":
-                mists_best = dungeon["dungeon"]
+                mists_best = dungeon
             elif dungeon["dungeon"] == "Halls of Atonement":
-                halls_best = dungeon["dungeon"]
+                halls_best = dungeon
             elif dungeon["dungeon"] == "Plaguefall":
-                pf_best = dungeon["dungeon"]
-            elif dungeon["dungeon"] == "Theatre of Pain":
-                top_best = dungeon["dungeon"]
+                pf_best = dungeon
+            elif dungeon["dungeon"] == "Theater of Pain":
+                top_best = dungeon
             elif dungeon["dungeon"] == "De Other Side":
-                dos_best = dungeon["dungeon"]
+                dos_best = dungeon
 
         # alternate dungeon data
         alt, nw_alt, sd_alt, halls_alt, plaguefall_alt, mists_alt, spires_alt, top_alt, dos_alt = request_json['mythic_plus_alternate_runs'], None, None, None, None, None, None, None, None
         for dungeon in alt:
             if dungeon["dungeon"] == "The Necrotic Wake":
-                nw_alt = dungeon["dungeon"]
+                nw_alt = dungeon
             elif dungeon["dungeon"] == "Sanguine Depths":
-                sd_alt = dungeon["dungeon"]
+                sd_alt = dungeon
             elif dungeon["dungeon"] == "Spires of Ascension":
-                spires_alt = dungeon["dungeon"]
+                spires_alt = dungeon
             elif dungeon["dungeon"] == "Mists of Tirna Scithe":
-                mists_alt = dungeon["dungeon"]
+                mists_alt = dungeon
             elif dungeon["dungeon"] == "Halls of Atonement":
-                halls_alt = dungeon["dungeon"]
+                halls_alt = dungeon
             elif dungeon["dungeon"] == "Plaguefall":
-                pf_alt = dungeon["dungeon"]
-            elif dungeon["dungeon"] == "Theatre of Pain":
-                top_alt = dungeon["dungeon"]
+                pf_alt = dungeon
+            elif dungeon["dungeon"] == "Theater of Pain":
+                top_alt = dungeon
             elif dungeon["dungeon"] == "De Other Side":
-                dos_alt = dungeon["dungeon"]
+                dos_alt = dungeon
 
-        character_embed = discord.Embed(title=f"{character_name}'s Raider.IO Profile", url=f"https://raider.io/characters/{realm}/{server}/{character_name}", 
+        character_embed = discord.Embed(title=f"{character_name}'s Raider.IO Profile", url=f"https://raider.io/characters/{region}/{realm}/{character_name}", 
             description="", color=discord.Color.blue()) # TODO: pair color with spec (shaman is blue, etc...)
 
         character_embed.set_author(name=f"Requested by {ctx.author.display_name}", icon_url=ctx.author.avatar_url)
+
+        character_embed.set_thumbnail(url=char_thumbnail)
 
         character_embed.add_field(name="Overall Statistics", value=f"**Total Score: {char_total_score}**\n", inline=False)
                                                                    # f"Tank Score: 0\n" +
@@ -135,12 +139,15 @@ class Individuals(commands.Cog):
                                                          f"Overall **{char_class}** Rank: Realm **#{char_class_rank['realm']}**, Region **#{char_class_rank['region']}**, World **#{char_class_rank['world']}**\n" +
                                                          f"Overall **{char_role}** Rank: Realm **#{char_role_rank['realm']}**, Region **#{char_role_rank['region']}**, World **#{char_role_rank['world']}**", inline=False)
 
-        # character_embed.add_field(name="**Best Dungeons**", value="\u200b", inline=False)
-
-        # character_embed.add_field(name="Dungeon Name", value="Mists of Tirna Scithe\nNW", inline=True)
-        # character_embed.add_field(name="Fortified", value="13\n15", inline=True)
-        # character_embed.add_field(name="Tyrannical", value="19\n11", inline=True)
-        # character_embed.add_field(name="Total Score", value="232.4\n101.3", inline=True)
+        # TODO: generalize this code with for loop and add cases for None best dungeons
+        character_embed.add_field(name="Best Dungeons", value=f"De Other Side - **{'+' * dos_best['num_keystone_upgrades']}{dos_best['mythic_level']}** {dos_best['affixes'][0]['name']}, Score: **{dos_best['score']:.1f}**\n" +
+                                                                  f"Halls of Atonement - **{'+' * halls_best['num_keystone_upgrades']}{halls_best['mythic_level']}** {halls_best['affixes'][0]['name']}, Score: **{halls_best['score']:.1f}**\n" +
+                                                                  f"Mists of Tirna Scithe - **{'+' * mists_best['num_keystone_upgrades']}{mists_best['mythic_level']}** {mists_best['affixes'][0]['name']}, Score: **{mists_best['score']:.1f}**\n" +
+                                                                  f"Necrotic Wake - **{'+' * nw_best['num_keystone_upgrades']}{nw_best['mythic_level']}** {nw_best['affixes'][0]['name']}, Score: **{nw_best['score']:.1f}**\n" +
+                                                                  f"Plaguefall - **{'+' * pf_best['num_keystone_upgrades']}{pf_best['mythic_level']}** {pf_best['affixes'][0]['name']}, Score: **{pf_best['score']:.1f}**\n" +
+                                                                  f"Sanguine Depths - **{'+' * sd_best['num_keystone_upgrades']}{sd_best['mythic_level']}** {sd_best['affixes'][0]['name']}, Score: **{sd_best['score']:.1f}**\n" +
+                                                                  f"Spires of Ascension - **{'+' * spires_best['num_keystone_upgrades']}{spires_best['mythic_level']}** {spires_best['affixes'][0]['name']}, Score: **{spires_best['score']:.1f}**\n" +
+                                                                  f"Theater of Pain - **{'+' * top_best['num_keystone_upgrades']}{top_best['mythic_level']}** {top_best['affixes'][0]['name']}, Score: **{top_best['score']:.1f}**\n", inline=False)
 
         await ctx.send(embed=character_embed)
 
